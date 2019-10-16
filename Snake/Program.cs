fd1f648e2e6e51a2459d3b891a65529ad89a5529
STORYM <ssashkaa01@gmail.com>
2019-10-16 23:43:41 +0300

    release

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -16,27 +16,57 @@ namespace Snake
     {
         public int x { get; set; } // X координата
         public int y { get; set; } // Y координата
+
+        public override string ToString()
+        {
+            return $"X: {x}, Y: {y}";
+        }
     }
 
     // Контролер координат
     class CoordinateHelper
     {
+        public static int maxHeight = 15;
+        public static int maxWidth = 15;
+
         // Перевірити чи це стіна
-        public static bool IsWall(Coordinate pos)
+        public static bool IsNotWall(Coordinate pos)
         {
-            return false;
+            return (pos.x < maxWidth && pos.y < maxHeight && pos.y > 0 && pos.x > 0);
         }
 
         // Перевірити чи це змія натрапила на себе
         public static bool IsSnake(Coordinate pos, ref List<Coordinate> coordinates)
         {
+            foreach(var item in coordinates)
+            {
+                if (item.x == pos.x && item.y == pos.y) return true;
+            }
+
+            return false;
+        }
+
+        // Перевірити чи це змія натрапила на себе
+        public static bool IsSnake(int x, int y, ref List<Coordinate> coordinates)
+        {
+            foreach (var item in coordinates)
+            {
+                if (item.x == x && item.y == y) return true;
+            }
+
             return false;
         }
 
         // Перевірити чи це їжа
         public static bool IsFood(Coordinate pos, Coordinate food)
         {
-            return false;
+            return (pos.x == food.x && pos.y == food.y);
+        }
+
+        // Перевірити чи це їжа
+        public static bool IsFood(int x, int y, Coordinate food)
+        {
+            return (x == food.x && y == food.y);
         }
     }
 
@@ -45,16 +75,18 @@ namespace Snake
     {
         private int screenHeight { get; set; }
         private int screenWidth { get; set; }
-        public char bodySnake { get; set; }     // Як відображається тіло змійки
-        public char wall { get; set; }          // Як відображається стіна
-        public char food { get; set; }          // Як відображається їжа
-        public char nothing { get; set; }       // Як відображається пусте місце
-     
+        public string bodySnake { get; set; }     // Як відображається тіло змійки
+        public string wall { get; set; }          // Як відображається стіна
+        public string food { get; set; }          // Як відображається їжа
+        public string nothing { get; set; }       // Як відображається пусте місце
+        public Coordinate foodCoordinate;
+        public List<Coordinate> snakeCoordinate;
+
         public ViewController()
         {
             //Встановлюємо розміри екрану
-            screenHeight = 20;
-            screenWidth = 50;
+            screenHeight = 25;
+            screenWidth = 60;
 
             Console.WindowHeight = screenHeight;
             Console.WindowWidth = screenWidth;
@@ -62,16 +94,60 @@ namespace Snake
             Console.SetBufferSize(screenWidth, screenHeight);
             Console.CursorVisible = false;
 
-            bodySnake = '*';
-            wall = '#';
-            food = '@';
-            nothing = ' ';
+            bodySnake = "* ";
+            wall = "# ";
+            food = "@ ";
+            nothing = "  ";
            
         }
 
         public void PlayingField()
         {
-            Console.WriteLine("Reload map");
+
+            Console.Clear();
+
+            for(int y = 0; y <= CoordinateHelper.maxHeight; y++)
+            {
+
+                for (int x = 0; x <= CoordinateHelper.maxWidth; x++)
+                {
+                    if (x == 0 || y == 0 || x == CoordinateHelper.maxWidth || y == CoordinateHelper.maxHeight)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                        Console.Write(wall);
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        
+                        if(CoordinateHelper.IsFood(x, y, foodCoordinate))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+
+                            Console.Write(food);
+
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if(CoordinateHelper.IsSnake(x, y, ref snakeCoordinate))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+
+                            Console.Write(bodySnake);
+
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else
+                        {
+                            Console.Write(nothing);
+                        }
+                    }
+
+                    
+                }
+                Console.WriteLine();
+            }
         }
         
         public void GameOver()
@@ -84,11 +160,24 @@ namespace Snake
     // Контролер змії
     class SnakeController
     {
-        List<Coordinate> coordinates;
+
+        public List<Coordinate> snake;
+        public Coordinate food;
 
         public SnakeController()
         {
-            coordinates = new List<Coordinate>();
+            snake = new List<Coordinate>()
+            {
+                new Coordinate(){ x = 10, y = 10 },
+                new Coordinate(){ x = 9, y = 10 },
+                new Coordinate(){ x = 8, y = 10 },
+            };
+
+            food = new Coordinate()
+            {
+                x = 4,
+                y = 4
+            };
         }
 
         public GO move { get; set; }
@@ -117,24 +206,80 @@ namespace Snake
             return false;
         }
 
+        private void GenerateFood()
+        {
+            Random rnd = new Random();
+
+            do
+            {
+                food.x = rnd.Next(0, CoordinateHelper.maxWidth);
+                food.y = rnd.Next(0, CoordinateHelper.maxHeight);
+
+            } while (CoordinateHelper.IsSnake(food, ref snake) || !CoordinateHelper.IsNotWall(food));
+        }
+
+        private bool SetNewCoordinate(Coordinate coordinate)
+        {
+           
+            if (!CoordinateHelper.IsNotWall(coordinate)) return false;
+
+            if (CoordinateHelper.IsSnake(coordinate, ref snake)) return false;
+
+            if (CoordinateHelper.IsFood(coordinate, food))
+            {
+                snake.Insert(0, food);
+                GenerateFood();
+                Moving();
+            }
+            else
+            {
+                snake.Insert(0, coordinate);
+                snake.RemoveAt(snake.Count - 1);
+            }
+
+            return true;
+        }
+
         public bool Up()
         {
-            return true;
+            Coordinate newcoordinate = new Coordinate()
+            {
+                x = snake.First().x,
+                y = snake.First().y - 1,
+            };
+
+            return SetNewCoordinate(newcoordinate);
         }
 
         public bool Down()
         {
-            return true;
+            Coordinate newcoordinate = new Coordinate()
+            {
+                x = snake.First().x,
+                y = snake.First().y + 1,
+            };
+            return SetNewCoordinate(newcoordinate);
         }
 
         public bool Left()
         {
-            return true;
+            Coordinate newcoordinate = new Coordinate()
+            {
+                x = snake.First().x - 1,
+                y = snake.First().y,
+            };
+
+            return SetNewCoordinate(newcoordinate);
         }
 
         public bool Right()
         {
-            return true;
+            Coordinate newcoordinate = new Coordinate()
+            {
+                x = snake.First().x + 1,
+                y = snake.First().y,
+            };
+            return SetNewCoordinate(newcoordinate);
         }
     }
 
@@ -152,13 +297,20 @@ namespace Snake
             snakeController = new SnakeController();
             viewController = new ViewController();
             stopWatch = new Stopwatch();
+            UpdatePostion();
+        }
+
+        public void UpdatePostion()
+        {
+            viewController.snakeCoordinate = snakeController.snake;
+            viewController.foodCoordinate = snakeController.food;
         }
 
         // Паралельно запускаємо паралельні цикли на переміщення і зчитування клавіш
         public void Play()
         {
             play = true;
-            timeout = 1000;
+            timeout = 700;
             stopWatch.Start();
 
             Parallel.Invoke(ReadKey, Moving);
@@ -181,6 +333,8 @@ namespace Snake
                     // Виконуємо переміщення
                     if (snakeController.Moving())
                     {
+                        UpdatePostion();
+
                         // Перемальовуємо поле
                         viewController.PlayingField();
                     }
@@ -228,6 +382,8 @@ namespace Snake
                     // Виконуємо переміщення
                     if (snakeController.Moving())
                     {
+                        UpdatePostion();
+
                         // Перемальовуємо поле
                         viewController.PlayingField();
                     }
